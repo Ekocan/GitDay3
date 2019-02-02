11ca7c89a4a0f601ab2ca77a605d35e5bcec6cf5
Ekrem <metrozi2018@Gmail.com>
2019-02-01 23:27:34 -0500

    comit push

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -28,13 +28,22 @@ public class LoginTests {
 
 
     @Test
-    public void logOutTest(){
+    public void logOutTest1(){
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
 
         driver.findElement(By.id("ctl00_logout")).click();
         Assert.assertEquals(driver.getTitle(),"Web Orders Login");
+    }
+    @Test
+    public void negativeloginTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2" + Keys.ENTER);
+        String errorMsg = driver.findElement(By.id("ctl00_MainContent_status")).getText();
+
+        Assert.assertEquals(errorMsg, "Invalid Login or Password.");
     }
 
     @AfterMethod
