6a1c566aa8612e7e59d4f7bd91b82ccab2d74c37
Ekrem <46767956+Ekocan@users.noreply.github.com>
2019-02-01 23:50:18 -0500

    logintest1 added

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -25,17 +25,17 @@ public class LoginTests {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-
-
+    
     @Test
-    public void logOutTest1(){
+    public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-
-        driver.findElement(By.id("ctl00_logout")).click();
-        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Web Orders");
     }
+
+  
     @Test
     public void negativeloginTest() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
@@ -46,6 +46,22 @@ public class LoginTests {
         Assert.assertEquals(errorMsg, "Invalid Login or Password.");
     }
 
+    
+    @Test
+    public void logOutTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        driver.findElement(By.id("ctl00_logout")).click();
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Web Orders Login");
+
+    }
+
+
+    
+    
     @AfterMethod
     public void cleanUp() {
         driver.close();
