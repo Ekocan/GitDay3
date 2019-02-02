a44d964d98e702ad5886c0ac2d8de83a0cfc54bf
Ekrem <metrozi2018@Gmail.com>
2019-02-01 22:27:13 -0500

    commit3

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,9 +4,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -15,7 +18,7 @@ public class LoginTests {
 
     WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -23,18 +26,28 @@ public class LoginTests {
     }
 
     @Test
-    public void loginTest1() {
+    public void negativeloginTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2" + Keys.ENTER);
+        String errorMsg = driver.findElement(By.id("ctl00_MainContent_status")).getText();
+
+        Assert.assertEquals(errorMsg, "Invalid Login or Password.");
+    }
+
+    @Test
+    public void logOutTest(){
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        
-        Assert.assertEquals(driver.getTitle(), "Web Orders");
+
+        driver.findElement(By.id("ctl00_logout")).click();
+        Assert.assertEquals(driver.getTitle(),"Web Orders Login");
     }
-    
+
     @AfterMethod
     public void cleanUp() {
         driver.close();
     }
-
 
 }
