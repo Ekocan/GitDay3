72475f921fd7daa2bd04fe594823b0f8ba1471d6
Ekrem <46767956+Ekocan@users.noreply.github.com>
2019-02-01 22:20:11 -0500

    Add AfterMethod

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -27,7 +27,14 @@ public class LoginTests {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
+    
+    @AfterMethod
+    public void cleanUp() {
+        driver.close();
+    }
+
 
 }
