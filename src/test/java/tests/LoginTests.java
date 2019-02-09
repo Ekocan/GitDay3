69c22d2433e3e91c6a2b01da2ebc6a3a1d37d52e
Ekrem <metrozi2018@Gmail.com>
2019-02-08 20:35:23 -0500

    add hashmap for fun

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -11,6 +11,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 //Login tests are maintained here.
 //without Page object approach
@@ -18,6 +20,7 @@ public class LoginTests {
 
     WebDriver driver;
 
+    Map<String,String > myMap= new HashMap<String, String>();
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
