50339300c0b8add2b4fbc0684e381fcf12fcff2f
Ekrem <metrozi2018@Gmail.com>
2019-02-08 20:02:25 -0500

    gitignore edded

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,7 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -13,7 +12,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-
+//Login tests are maintained here.
+//without Page object approach
 public class LoginTests {
 
     WebDriver driver;
@@ -26,7 +26,6 @@ public class LoginTests {
     }
 
 
-    
     @Test
     public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
@@ -35,7 +34,6 @@ public class LoginTests {
         String title = driver.getTitle();
         Assert.assertEquals(title, "Web Orders");
     }
-
 
     @Test
     public void negativeloginTest() {
@@ -48,19 +46,20 @@ public class LoginTests {
     }
 
 
-        @Test
-        public void logOutTest() {
-            driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-            driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
-            driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+    @Test
+    public void logOutTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
 
-            driver.findElement(By.id("ctl00_logout")).click();
-            String title = driver.getTitle();
-            Assert.assertEquals(title, "Web Orders Login");
+        driver.findElement(By.id("ctl00_logout")).click();
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Web Orders Login");
 
-        }
+    }
 
-        @AfterMethod
+
+    @AfterMethod
     public void cleanUp() {
         driver.close();
     }
